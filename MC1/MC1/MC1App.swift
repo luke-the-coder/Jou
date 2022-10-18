5dbc47090c5697cb9e4c76492dbcd24fae70f684
luke-the-coder <94640753+luke-the-coder@users.noreply.github.com>
2022-10-18 12:43:23 +0200

    Added first modal view, no logics yet only structure.

---
+++ MC1/MC1/MC1App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1App.swift
+//  MC1
+//
+//  Created by Fabio Luca Uccella on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MC1/MC1App.swift
@@ -0,0 +1,15 @@
+//
+//  MC1App.swift
+//  MC1
+//
+
+import SwiftUI
+
+@main
+struct MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
